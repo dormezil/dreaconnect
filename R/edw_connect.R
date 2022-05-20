3e55afd8a9e269979ada17af4f13c5a4826f007d
Stevens Dormezil <stevens.dormezil@palmbeachschools.org>
2022-05-20 13:03:53 -0400

    Initial commit

---
+++ R/edw_connect.R
@@ -0,0 +1,13 @@
+#' @title EDW Connection Function
+#'
+#' @description This function connects to the edw database
+#'
+#' @return A string connection to Microsoft SQL Server
+#' @export
+#'
+#' @examples
+#' edw_connect()
+
+edw_connect <- function(){
+  DBI::dbConnect(odbc::odbc(), "SDM_MSSQL", timeout = 10)
+}
